e6c54eab9db217929dacf7e511fdfcbe3614e43e
nealcpeters <nealcpeters@gmail.com>
2014-02-19 16:51:47 -0600

    add ajax

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,11 +5,12 @@ end
 
 # TODO: convert this route to use AJAX
 post '/rolls' do
+  p params
   # If the user passes-in a "value", let's use it. Otherwise, we'll generate a random one.
   # See: roll_if_value_is_nil method in the Roll model.
   value = params[:value] ? params[:value].to_i : nil
 
   @roll = value ? Roll.create({ value: value }) : Roll.create
 
-  erb :index  # HINT: what does this do? what should we do instead?
+  erb :_url, layout: false
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
+  $('form').on("submit", function(event){
+    event.preventDefault();
+    var randomNumber = Math.floor((Math.random()*6)+1);
+    var url = $(this).attr("action")
 
-  // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
-  //   3- generate a random number between 1 and 6 using JavaScript
-  //   4- use jQuery to submit an AJAX post to the form's action
-  //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
+    $.post( url, {value: randomNumber}, function(data) {
 
+    })
+      .done(function(serverResponse, status, request) {
+        $("#die").html(serverResponse);
+      });
+  })
 });
